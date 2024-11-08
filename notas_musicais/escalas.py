1df09365e946ada5065ea2efe15768e4737edc3a
armarjr <armarcm.job@gmail.com>
2024-11-08 12:36:02 -0300

    implementação inicial do CLI

--- notas_musicais/escalas.py
+++ notas_musicais/escalas.py
@@ -3,38 +3,38 @@ ESCALAS = {'maior': (0, 2, 4, 5, 7, 9, 11)}
 
 
 def escala(tonica: str, tonalidade: str) -> dict[str, list[str]]:
-    '''
+    """
     Gera uma escala apartir de uma tonica e uma tonalidade
-    
+
     Parameters:
         tonica: nota que será a tonica da escala
         tonalidade: tonalidade da escala
-    
+
     returns:
         Um dicionario com as notas da escala e os graus
-    
+
     Raises:
         ValueError : caso a tonica nao seja uma nota valida
-        KeyError : caso a escala nao exista ou nao tenha sido implementada  
-         
+        KeyError : caso a escala nao exista ou nao tenha sido implementada
+
     Examples:
         >>> escala('C', 'maior')
         {'notas': ['C', 'D', 'E', 'F', 'G', 'A', 'B'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
 
         >>> escala('a', 'maior')
         {'notas': ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
-    
-    '''
-    
+
+    """
     tonica = tonica.upper()
     try:
         tonica_pos = NOTAS.index(tonica)
         intervalos = ESCALAS[tonalidade]
     except ValueError:
-        raise ValueError(f'Esta nota não existe, tente uma dessas {NOTAS}')        
-    except KeyError:    
+        raise ValueError(f'Esta nota não existe, tente uma dessas {NOTAS}')
+    except KeyError:
         raise KeyError(
-            f'Esta escala não existe ou nao foi implementada, tente uma dessas {list(ESCALAS.keys())}'
+            'Esta escala não existe ou nao foi implementada, '
+            f'tente uma dessas {list(ESCALAS.keys())}'
         )
     temp = []
     for intervalo in intervalos:

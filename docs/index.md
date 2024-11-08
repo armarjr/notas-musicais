9ed1128819647bf373cd00904e4f6cffdc2e7c2a
armarjr <armarcm.job@gmail.com>
2024-11-08 12:36:02 -0300

    implementação inicial do CLI

--- docs/index.md
+++ docs/index.md
@@ -1,2 +1,71 @@
 ![Logo do Projeto](assets/logo.png){ width="200" .center}
 # Notas Musicais
+
+
+## Como usar:
+
+Voce pode chamar escalas via linha de terminal. Por exemplo:
+
+```bash
+
+poetry run escalas
+```
+
+Retornando os graus e as notas correspondentes a essa escala :
+
+```
+
+┏━━━┳━━━━┳━━━━━┳━━━━┳━━━┳━━━━┳━━━━━┓
+┃ I ┃ II ┃ III ┃ IV ┃ V ┃ VI ┃ VII ┃
+┡━━━╇━━━━╇━━━━━╇━━━━╇━━━╇━━━━╇━━━━━┩
+│ C │ D  │ E   │ F  │ G │ A  │ B   │
+└───┴────┴─────┴────┴───┴────┴─────┘
+```
+
+### Alteraçao na escala
+
+O primeiro parametro do CLI é a tonica da escala que deseja exibir. desta forma voce pode alterar a escala a ser exibida. Por exemplo escala de F#
+
+```bash
+poetry run escalas F#
+```
+Retorna :
+
+```
+┏━━━━┳━━━━┳━━━━━┳━━━━┳━━━━┳━━━━┳━━━━━┓
+┃ I  ┃ II ┃ III ┃ IV ┃ V  ┃ VI ┃ VII ┃
+┡━━━━╇━━━━╇━━━━━╇━━━━╇━━━━╇━━━━╇━━━━━┩
+│ F# │ G# │ A#  │ B  │ C# │ D# │ F   │
+└────┴────┴─────┴────┴────┴────┴─────┘
+
+```
+
+## Voce pode alterar a tonalidade da escala. Este é o segundo parametro da linha de comando. Por exemplo a escala de 'D#' maior
+
+```bash
+poetry run escalas D# maior
+
+```
+Retornando:
+```
+┏━━━━┳━━━━┳━━━━━┳━━━━┳━━━━┳━━━━┳━━━━━┓
+┃ I  ┃ II ┃ III ┃ IV ┃ V  ┃ VI ┃ VII ┃
+┡━━━━╇━━━━╇━━━━━╇━━━━╇━━━━╇━━━━╇━━━━━┩
+│ D# │ F  │ G   │ G# │ A# │ C  │ D   │
+└────┴────┴─────┴────┴────┴────┴─────┘
+
+```
+## Mais informaçoes sobre o CLI
+Para descobrir outras opçoes, voce pode usar a flag `--help` :
+
+```bash
+poetry run escalas --help
+
+
+ Usage: escalas [OPTIONS] [TONICA] [TONALIDADE]                                                                 
+                                                                                                                
+╭─ Arguments ──────────────────────────────────────────────────────────────────────────────────────────────────╮
+│   tonica          [TONICA]      Tonica da escala [default: c]                                                │
+│   tonalidade      [TONALIDADE]  Tonalidade da escala [default: maior]                                        │
+╰──────────────────────────────────────────────────────────────────────────────────────────────────────────────╯
+```
--- tests/test_escalas.py
+++ tests/test_escalas.py
@@ -1,42 +1,48 @@
-"""
-AAA - 3A  - A3 
-    
-Arange - Act - Assets
-Arrumar - Agir - Garantir   
-"""
-from notas_musicais.escalas import ESCALAS, NOTAS, escala
+# AAA - 3A  - A3
+# Arange - Act - Assets
+# Arrumar - Agir - Garantir
 from pytest import mark, raises
+
+from notas_musicais.escalas import ESCALAS, NOTAS, escala
+
 
 def test_deve_funcionar_com_letras_minusculas():
     # Arrumar
     tonica = 'c'
+
     tonalidade = 'maior'
-    
-    # Act  chama o que testar    
+
+    # Act  chama o que testar
     result = escala(tonica, tonalidade)
-    
-    # Assert 
+
+    # Assert
     assert result
 
+
 def test_deve_retornar_um_erro_dizendo_que_a_nota_nao_existe():
-    tonica = "X"
-    tonalidade = "maior"
-     
+    tonica = 'X'
+
+    tonalidade = 'maior'
+
     mensagem_de_erro = f'Esta nota não existe, tente uma dessas {NOTAS}'
-    
+
     with raises(ValueError) as error:
         escala(tonica, tonalidade)
-        
+
     assert mensagem_de_erro == error.value.args[0]
-    
+
+
 def test_deve_retornar_um_erro_dizendo_que_a_escala_nao_existe():
-    tonica = "c"
-    tonalidade = "tonalidade"
-     
+    tonica = 'c'
+
+    tonalidade = 'tonalidade'
+
     mensagem_de_erro = f'Esta escala não existe ou nao foi implementada, tente uma dessas {list(ESCALAS.keys())}'
     with raises(KeyError) as error:
         escala(tonica, tonalidade)
-    assert mensagem_de_erro == error.value.args[0]    
+
+    assert mensagem_de_erro == error.value.args[0]
+
 
 @mark.parametrize(
     'tonica,esperado',
@@ -44,21 +50,22 @@ def test_deve_retornar_um_erro_dizendo_que_a_escala_nao_existe():
         ('C', ['C', 'D', 'E', 'F', 'G', 'A', 'B']),
         ('C#', ['C#', 'D#', 'F', 'F#', 'G#', 'A#', 'C']),
         ('F', ['F', 'G', 'A', 'A#', 'C', 'D', 'E']),
-    ],    
+    ],
 )
 def teste_deve_retornar_as_notas_corretas(tonica, esperado):
-    tonalidade = 'maior'    
+    tonalidade = 'maior'
     result = escala(tonica, tonalidade)
     assert result['notas'] == esperado
-    
+
+
 def teste_deve_retornar_os_sete_graus():
-    
+
     tonica = 'c'
-    
+
     tonalidade = 'maior'
-    
+
     esperado = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']
-    
+
     result = escala(tonica, tonalidade)
-    
+
     assert result['graus'] == esperado
